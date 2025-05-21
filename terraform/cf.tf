02466d704c4eb402c9f7deac7e262975fd87c58a
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-05-21 09:21:53 +0000

    Test support-plan page.  Add 404 page.  Use 404 page in CF. Add security headers. Update TF. Update AWS provider.
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/cf.tf
+++ terraform/cf.tf
@@ -18,10 +18,11 @@ resource "aws_cloudfront_distribution" "cdn" {
   aliases = ["getclarity.win", "www.getclarity.win"]
 
   default_cache_behavior {
-    target_origin_id       = "s3-site"
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
+    target_origin_id           = "s3-site"
+    viewer_protocol_policy     = "redirect-to-https"
+    allowed_methods            = ["GET", "HEAD"]
+    cached_methods             = ["GET", "HEAD"]
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
     forwarded_values {
       query_string = false
       cookies {
@@ -40,5 +41,49 @@ resource "aws_cloudfront_distribution" "cdn" {
     geo_restriction {
       restriction_type = "none"
     }
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
+    error_caching_min_ttl = 0
+  }
+
+  # Optional — also catch 403s (common with private S3 buckets)
+  custom_error_response {
+    error_code            = 403
+    response_code         = 404
+    response_page_path    = "/404.html"
+    error_caching_min_ttl = 0
+  }
+
+}
+
+resource "aws_cloudfront_response_headers_policy" "security_headers" {
+  name = "ClaritySecurityHeaders"
+
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+
+    frame_options {
+      frame_option = "DENY"
+      override     = true
+    }
+
+    referrer_policy {
+      referrer_policy = "strict-origin-when-cross-origin"
+      override        = true
+    }
+
+    strict_transport_security {
+      access_control_max_age_sec = 63072000
+      include_subdomains         = true
+      preload                    = true
+      override                   = true
+    }
+
   }
 }
