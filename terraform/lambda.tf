6238c2f62ac4650433baf712d5218d054c42e4be
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-05-21 13:47:41 +0000

    has lambda so it updates in AWS
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,14 +1,15 @@
 resource "aws_lambda_function" "waitlist" {
-  function_name = "waitlistHandler"
-  role          = aws_iam_role.lambda_exec.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  filename      = "../lambda.zip"
+  function_name    = "waitlistHandler"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  filename         = "../lambda/lambda.zip"
+  source_code_hash = filebase64sha256("../lambda/lambda.zip")
 
   environment {
     variables = {
       TABLE_NAME   = aws_dynamodb_table.waitlist.name
-      SENDER_EMAIL = "noreply@clarity.local" # Replace when you verify your sender
+      SENDER_EMAIL = "hello@getclarity.win"
     }
   }
 
