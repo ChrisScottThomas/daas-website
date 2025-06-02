939e9c313d026b7f71c9400904e9e1007d42f1d3
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-02 07:03:39 +0000

    Create new lambda & eventbridge for retrying fails email sends.  Split workflows. Rename zips.
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,8 +3,34 @@ resource "aws_lambda_function" "waitlist" {
   role             = aws_iam_role.lambda_exec.arn
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "../lambda.zip"
-  source_code_hash = filebase64sha256("../lambda.zip")
+  filename         = "../waitlist-lambda.zip"
+  source_code_hash = filebase64sha256("../waitlist-lambda.zip")
+
+  environment {
+    variables = {
+      TABLE_NAME          = aws_dynamodb_table.waitlist.name
+      SENDER_EMAIL        = "hello@getclarity.win"
+      AIRTABLE_SECRET_ARN = data.aws_secretsmanager_secret.airtable_token.arn
+      AIRTABLE_BASE_ID    = "appK1h4aAzYAGEjTB"
+      AIRTABLE_TABLE_NAME = "Signups"
+    }
+  }
+  tags = {
+    Project = "Clarity"
+    Purpose = "WaitlistEmails"
+  }
+
+
+}
+
+resource "aws_lambda_function" "waitlist_retry" {
+  function_name = "waitlistRetryHandler"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "retry.handler"
+  runtime       = "nodejs18.x"
+
+  filename         = "../lambda/retry-lambda.zip"
+  source_code_hash = filebase64sha256("../lambda/retry-lambda.zip")
 
   environment {
     variables = {
@@ -16,4 +42,8 @@ resource "aws_lambda_function" "waitlist" {
     }
   }
 
+  tags = {
+    Project = "Clarity"
+    Purpose = "RetryFailedEmails"
+  }
 }
