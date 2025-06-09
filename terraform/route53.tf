957b025233cfae7ea0072f482bd1b81847165ccf
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-09 19:27:40 +0000

    Initiate move to MS365 for user email.
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -59,7 +59,7 @@ resource "aws_route53_record" "spf" {
   type    = "TXT"
   ttl     = 300
   records = [
-    "v=spf1 include:_spf.google.com include:amazonses.com ~all"
+    "v=spf1 include:spf.protection.outlook.com include:amazonses.com ~all"
   ]
 }
 
@@ -81,20 +81,20 @@ resource "aws_route53_record" "mail_from_spf" {
   records = ["v=spf1 include:amazonses.com ~all"]
 }
 
-resource "aws_route53_record" "gmail_mx" {
-  zone_id = aws_route53_zone.clarity.zone_id
-  name    = aws_route53_zone.clarity.name
-  type    = "MX"
-  ttl     = 300
-  records = [
-    "1 SMTP.GOOGLE.COM.",
-    "1 ASPMX.L.GOOGLE.COM.",
-    "5 ALT1.ASPMX.L.GOOGLE.COM.",
-    "5 ALT2.ASPMX.L.GOOGLE.COM.",
-    "10 ALT3.ASPMX.L.GOOGLE.COM.",
-    "10 ALT4.ASPMX.L.GOOGLE.COM."
-  ]
-}
+# resource "aws_route53_record" "gmail_mx" {
+#   zone_id = aws_route53_zone.clarity.zone_id
+#   name    = aws_route53_zone.clarity.name
+#   type    = "MX"
+#   ttl     = 300
+#   records = [
+#     "1 SMTP.GOOGLE.COM.",
+#     "1 ASPMX.L.GOOGLE.COM.",
+#     "5 ALT1.ASPMX.L.GOOGLE.COM.",
+#     "5 ALT2.ASPMX.L.GOOGLE.COM.",
+#     "10 ALT3.ASPMX.L.GOOGLE.COM.",
+#     "10 ALT4.ASPMX.L.GOOGLE.COM."
+#   ]
+# }
 
 resource "aws_route53_record" "mail_from_mx" {
   zone_id = aws_route53_zone.clarity.zone_id
@@ -112,7 +112,8 @@ resource "aws_route53_record" "google_verification" {
   type    = "TXT"
   ttl     = 300
   records = [
-    "google-site-verification=3lFFbJbfvZLdQS1LKuxAYhF4kCMNGGyEMtNhkzysSfo"
+    "google-site-verification=3lFFbJbfvZLdQS1LKuxAYhF4kCMNGGyEMtNhkzysSfo",
+    "MS=ms73991829"
   ]
   depends_on = [aws_route53_zone.clarity]
 }
@@ -135,4 +136,44 @@ resource "aws_route53_record" "google_dkim" {
     "GmjkcozUCyr9x8fmqhzlfPRDaQKSnWt/885KLQuQhJDTp7uYLZ+9V48D7JD/WXE+qOzemeNnkE12xoKuig7PlcVcjUvKG6C9a7L9Qugs57HJakPN4Z1ku3N9BKq5Q7/3ib0kvtvUC2uVAz9yaeIkk443dx/zKib",
     "lKYp8KElpq6LJsHqLf3CjduQ00vBx2vmXo14lIvRGU6UCOU2B/4C+DbX5HrqIzzXThMzigPoMorCfh2UIzDyosz9cDW+Dp/pHiI6ScET3qHB2Yiu6kfHhPFDvnRnokbwOqf+wIDAQAB"
   ]
+}
+
+resource "aws_route53_record" "m365_mx" {
+  zone_id = aws_route53_zone.clarity.zone_id
+  name    = "getclarity.win"
+  type    = "MX"
+  ttl     = 300
+  records = ["0 getclarity-win.mail.protection.outlook.com."]
+}
+
+resource "aws_route53_record" "m365_dkim1" {
+  zone_id = aws_route53_zone.clarity.zone_id
+  name    = "selector1._domainkey"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["selector1-getclarity-win._domainkey.claritysupportservices.e-v1.dkim.mail.microsoft."]
+}
+
+resource "aws_route53_record" "m365_dkim2" {
+  zone_id = aws_route53_zone.clarity.zone_id
+  name    = "selector2._domainkey"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["selector2-getclarity-win._domainkey.claritysupportservices.e-v1.dkim.mail.microsoft."]
+}
+
+resource "aws_route53_record" "autodiscover" {
+  zone_id = aws_route53_zone.clarity.zone_id
+  name    = "autodiscover.getclarity.win"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["autodiscover.outlook.com"]
+}
+
+resource "aws_route53_record" "m365_srv" {
+  zone_id = aws_route53_zone.clarity.zone_id
+  name    = "_sip._tls.getclarity.win"
+  type    = "SRV"
+  ttl     = 300
+  records = ["100 1 443 sipdir.online.lync.com."]
 }
