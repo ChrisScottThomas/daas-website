c389fda8e0c7203fad69f85683b0d7b70a71d01b
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-13 16:59:40 +0000

    declare secret version
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -5,3 +5,7 @@ data "aws_secretsmanager_secret" "airtable_token" {
 data "aws_secretsmanager_secret" "stripe_key" {
   name = "clarity-stripe-key"
 }
+
+data "aws_secretsmanager_secret_version" "stripe_key" {
+  secret_id = "clarity-stripe-key"
+}
