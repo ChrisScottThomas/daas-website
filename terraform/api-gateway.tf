f8cdc24e9ba980f2749b5a35fe1859110a61ad35
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-13 15:55:00 +0000

    fix dependency issue
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -286,8 +286,12 @@ resource "aws_api_gateway_integration" "stripe_checkout_options" {
 resource "aws_api_gateway_method_response" "stripe_checkout_options_response" {
   rest_api_id = aws_api_gateway_rest_api.waitlist_api.id
   resource_id = aws_api_gateway_resource.stripe_checkout.id
-  http_method = "OPTIONS"
+  http_method = aws_api_gateway_method.stripe_checkout_options.http_method
   status_code = "200"
+
+  depends_on = [
+    aws_api_gateway_method.stripe_checkout_options
+  ]
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true
@@ -296,15 +300,25 @@ resource "aws_api_gateway_method_response" "stripe_checkout_options_response" {
   }
 }
 
+
 resource "aws_api_gateway_integration_response" "stripe_checkout_options_response" {
   rest_api_id = aws_api_gateway_rest_api.waitlist_api.id
   resource_id = aws_api_gateway_resource.stripe_checkout.id
   http_method = aws_api_gateway_method.stripe_checkout_options.http_method
   status_code = "200"
 
+  depends_on = [
+    aws_api_gateway_integration.stripe_checkout_options,
+    aws_api_gateway_method_response.stripe_checkout_options_response
+  ]
+
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
   }
 }
