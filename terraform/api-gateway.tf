a7d1c24bd86c6337998b34ad899bd376bce112c1
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-15 14:26:12 +0000

    Fix render, fix lambda resource name
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -453,13 +453,13 @@ resource "aws_api_gateway_integration" "select_plan" {
   http_method             = aws_api_gateway_method.select_plan_get.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.select_plan.invoke_arn
+  uri                     = aws_lambda_function.select_plan_lambda.invoke_arn
 }
 
 resource "aws_lambda_permission" "select_plan_apigw" {
   statement_id  = "AllowInvokeSelectPlanFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.select_plan.function_name
+  function_name = aws_lambda_function.select_plan_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.waitlist_api.execution_arn}/*/*"
 }
