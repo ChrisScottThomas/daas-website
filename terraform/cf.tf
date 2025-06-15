0c65c507a820fa3a776bb259416e3b57c4300a0f
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-15 20:19:17 +0000

    revert to s3 static site
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/cf.tf
+++ terraform/cf.tf
@@ -2,11 +2,14 @@ resource "aws_cloudfront_distribution" "cdn" {
   depends_on = [aws_s3_bucket.site]
 
   origin {
-    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    domain_name = aws_s3_bucket_website_configuration.site.website_endpoint
     origin_id   = "s3-site"
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,12 +3,24 @@ resource "aws_s3_bucket" "site" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket                  = aws_s3_bucket.site.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "site_policy" {
@@ -17,17 +29,11 @@ resource "aws_s3_bucket_policy" "site_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
-        },
-        Action   = ["s3:GetObject"],
-        Resource = ["${aws_s3_bucket.site.arn}/*"]
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject"],
+        Resource  = ["${aws_s3_bucket.site.arn}/*"]
       }
     ]
   })
-}
-
-resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "Clarity CDN OAI"
 }
