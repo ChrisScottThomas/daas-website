7b4d979bf716db39c8ca11abde07bbe2df464304
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-15 19:32:47 +0000

    always use trailing slash
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -9,5 +9,5 @@ export default defineConfig({
   site: 'https://getclarity.win',
   integrations: [sitemap(), react()],
   output: "static",
-  trailingSlash: 'never',
+  trailingSlash: 'always',
 });
