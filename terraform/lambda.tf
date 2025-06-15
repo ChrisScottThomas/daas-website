ed5ef7fb500b06ca941d9a5a21882311832cd8ef
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-15 16:19:55 +0000

    Use secret name
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -144,7 +144,7 @@ resource "aws_lambda_function" "select_plan_lambda" {
 
   environment {
     variables = {
-      TOKEN_SECRET = data.aws_secretsmanager_secret_version.token_secret_version.secret_string
+      TOKEN_SECRET = data.aws_secretsmanager_secret.token_secret.name
     }
   }
 
