59eed9caa50f984d3b17593d68b83c786708026a
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-06-15 11:06:55 +0000

    install server adapter
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 
@@ -9,4 +10,5 @@ export default defineConfig({
   site: 'https://getclarity.win',
   integrations: [sitemap(), react()],
   output: "server",
+  adapter: node({ mode: 'standalone' }), // Enables self-contained output
 });
